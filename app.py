3f6837f6edc64ae06b492dd750c6e2ada845d9f2
Hari-7822 <h2608549@gmail.com>
2022-11-06 21:38:08 +0530

    login_page

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__, template_folder='template')
 
@@ -14,25 +15,30 @@ db = SQLAlchemy(app)
 
 class rec(db.Model):
 
-    id = db.Column("id", db.Integer, primary_key=True)  
-    name = db.Column("name", db.String(100), nullable = False)
-    role = db.Column("role", db.String(80), nullable = False)
+
+    name = db.Column("user_name", db.String(100), nullable = False)
+    phone = db.Column("user_number", db.String(50), nullable = False)
+    email = db.Column("user_email", db.String(100), nullable = False)
+    userid = db.Column("user_id", db.String(60), primary_key=True, nullable = False)  
+    password = db.Column('Password', db.String(100), nullable = False)
 
     def __ref__(self):
-        return f'<Welcome {self.role} {self.name} >'
+        return f'<Welcome {self.userid} {self.name} >'
 
 
 @app.route('/register', methods = ('POST', 'GET'))
 def reg():
 
     if request.method == 'POST':
-        id1 = request.form['id']
-        name1 = request.form['name']
-        role1 = request.form['role']
+        name_register = request.form['name']
+        phone_register = request.form['tele']
+        email_register = request.form['email']
+        userid_register = request.form['username']
+        password_register = request.form['password']
 
-        st = rec(id = id1, name = name1, role = role1)
+        new = rec(name = name_register, phone = phone_register, email = email_register, userid = userid_register, password = password_register)
 
-        db.session.add(st)
+        db.session.add(new)
         db.session.commit()
 
         return redirect(url_for('login'))
@@ -42,23 +48,24 @@ def reg():
 def login():
 
     if request.method == "POST":
-        user = request.form.get['username']
-        roles = request.form.get['role']
+        user = request.form['username']
+        passkey = request.form['password']
 
-        chk_name = user.query.filter(name = user).first()
-        chk_role = user.query.filter(role = roles).first()
+        chk_name = rec.query.filter_by(userid = user).first()        
         
-        
+        if not user or not check_password_hash(user.password, passkey):
+            flash('please check your username or password')
+            return "welcome" 
 
         return render_template('login.html.j2')
 
-    return render_template('base.html')
+    return render_template('login.html.j2')
 
 
 
 @app.route('/<usr>')
 def index(usr):
-    return render_template('base.html', usr = name)
+    return render_template('base.html')
 
 if __name__ == "__main__":
     db.create_all()
